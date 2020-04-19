bbd44f1c3e98b0575bd64732191c66f0d73cecf0
Vanja-Srivastava <52224065+Vanja-Srivastava@users.noreply.github.com>
2020-04-19 19:16:58 -0400

    changes for heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/ass7'));
+app.use(express.static(__dirname + '/dist/Ass7'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+
-        '/dist/ass7/index.html'));});
+        '/dist/Ass7/index.html'));});
 app.listen(process.env.PORT || 8080);
